eee90a128ab18aff65f372a61e817b833a9fe583
Corey Shafer <crshafer2001@gmail.com>
2022-08-03 22:21:44 -0400

    add ref

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Header from "./components/Header/header";
 import { useRef } from "react";
 
 function App() {
+	const headerRef = useRef();
 	return (
 		<div className="app">
-			<Header />
+			<Header refProp={headerRef} />
 		</div>
 	);
 }

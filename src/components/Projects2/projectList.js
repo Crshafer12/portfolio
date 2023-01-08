703761332ddef804e67d968dea4d3e1ae70e0dc7
Corey Shafer <crshafer2001@gmail.com>
2023-01-08 11:38:46 -0500

    project descriptions

--- src/components/Projects2/projectList.js
+++ src/components/Projects2/projectList.js
@@ -2,7 +2,7 @@ import React from "react";
 export const data = [
 	{
 		title: "Gradebook",
-		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		description: "A student information center designed to give students all the latesst updates with classes and teachers",
 		imgAddress: require("../../images/gradebook/2.PNG"),
 		githubLink: "https://github.com/Crshafer12/portfolio",
 		demoLink: "https://gradebook-089g.onrender.com",
@@ -10,7 +10,7 @@ export const data = [
 	},
 	{
 		title: "Keeper App",
-		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		description: "A simple note taking app that easily adds and displays any notes a user has.",
 		imgAddress: require("../../images/keeper/3.PNG"),
 		githubLink: "https://github.com/Crshafer12/portfolio",
 		demoLink: "https://keeperapp.onrender.com/",

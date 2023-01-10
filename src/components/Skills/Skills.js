40ada4b0825e6f3f4c053300233b415031f158f7
Corey Shafer <crshafer2001@gmail.com>
2023-01-10 13:57:12 -0500

    clean up

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -19,54 +19,7 @@ function Skills({ refProp }) {
 				</div>
 				<div className="skillsHeaderRight">Experienced in building modern web applications and reusable user interface components using React.js, Javascript, HTML, and CSS.</div>
 			</header>
-			{/* <div className="skillsBody">
-				<div className="skillsBodyLeft">
-					<div className="skillsBodyLeftHeader">Languages and Frameworks</div>
-					<div className="skillsBodyLeftItems">
-						<div className="skillItem">
-							<img src={HTML} alt="html"></img>
-							<span>HTML5</span>
-						</div>
-						<div className="skillItem">
-							<img src={css} alt="css"></img>
-							<span>CSS3</span>
-						</div>
-						<div className="skillItem">
-							<img src={js} alt="javascript"></img>
-							<span>Javascript (ES6)</span>
-						</div>
-						<div className="skillItem">
-							<img src={react} alt="react"></img>
-							<span>React</span>
-						</div>
-						<div className="skillItem">
-							<img src={node} alt="node,js"></img>
-							<span>Node.js</span>
-						</div>
-						{/* <div className="skillItem">
-							<img src={EJS} alt="EJS"></img>
-							<span>EJS</span>
-						</div> */}
-			{/* <div className="skillItem">
-							<img src={mongoDB} alt="EJS"></img>
-							<span>MongoDB</span>
-						</div>
-					</div> */}
-			{/* </div>
-				<div className="skillsBodyRight">
-					<div className="skillsBodyRightHeader">Development Tools</div>
-					<div className="skillsBodyRightItems">
-						<div className="skillItem">
-							<img src={git} alt="git"></img>
-							<span>Git</span>
-						</div>
-						<div className="skillItem">
-							<img src={github} alt="github"></img>
-							<span>Github</span>
-						</div>
-					</div>
-				</div> */}
-			{/* </div> */}
+
 			<div className="skillsSection" ref={refProp}>
 				<div className="skillsItems">
 					<div className="skillHeight">
